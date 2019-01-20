e97c214d856caa8d49669aae2ec59e75ff42cf2f
Nate Boyette <nateboyette@icloud.com>
2019-01-19 23:10:12 -0800

    Changed id variable in Movie.js to grab the id from the  Movie Route

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -25,7 +25,7 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <Link to={`/movies/${movie.id}`}>
+          <Link to={`/movie/${movie.id}`}>
             <MovieDetails key={movie.id} movie={movie} />
           </Link>
         ))}
