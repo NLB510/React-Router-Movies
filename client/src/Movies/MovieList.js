b934fc2780a429d0ef4fdddf2387b9e8795afc41
Nate Boyette <nateboyette@icloud.com>
2019-01-22 23:18:15 -0800

    Refactored Movie and MovieList components to be more dry. Added code to the MovieCard Component.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
+
+import MovieCard from "./MovieCard";
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -22,36 +24,22 @@ export default class MovieList extends Component {
   }
 
   render() {
+    const movieCard = this.state.movies.map(movie => {
+      return movie
+    })
+
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
           <Link to={`/movie/${movie.id}`}>
-            <MovieDetails key={movie.id} movie={movie} />
+            <MovieCard key={movie.id} movie={movie} id={movie.id} />
           </Link>
         ))}
+
+        
       </div>
     );
   }
 }
 
-function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
